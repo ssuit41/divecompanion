874dab6a1c8933b8ed1c89fe3b154a09ed31ad34
ClaudiusChristian02 <Claudius02@gmail.com>
2015-03-21 14:54:19 -0700

    Comment
    expressing concerns about finishing on time

--- home.php
+++ home.php
@@ -9,6 +9,8 @@ echo '<h3>Recent Posts</h3>';
 echo '<h3>Popular Sites</h3>';
 //Display 10-15 sites with the most hits/week or month
 //Possibly allow for an option to focus by geographical region
+//there are a lot of little features here. I'm concerned whether we can get the website done on time
+//Is there a core minimum feature set we can create?
 
 include 'footer.php';
 ?>
